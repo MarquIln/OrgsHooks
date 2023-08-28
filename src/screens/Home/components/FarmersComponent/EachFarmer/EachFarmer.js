d5d23f0492c7e5a70364da0f1ffa3fe1ba2726bc
Marcos <marcosraach.1@gmail.com>
2023-08-27 23:57:15 -0300

    using useReducer

--- src/screens/Home/components/FarmersComponent/EachFarmer/EachFarmer.js
+++ src/screens/Home/components/FarmersComponent/EachFarmer/EachFarmer.js
@@ -1,15 +1,19 @@
-import React, { useState } from 'react';
+/* eslint-disable prettier/prettier */
+import React, { useReducer } from 'react';
 import { Text, View, Image, TouchableOpacity } from 'react-native';
 import Stars from '../../../../../components/Stars';
 import { EachFarmerStyles } from './styles';
 
 export default function EachFarmer({ name, image, distance, stars }) {
-    const [ selected, setSelected ] = useState(false);
+    const [ selected, dispatch ] = useReducer(
+        (selected) => !selected,
+        false
+    );
 
     return <TouchableOpacity
-                style = {  EachFarmerStyles.card }
-                onPress = {() => setSelected(!selected)}
-        >
+            style = {  EachFarmerStyles.card }
+            onPress = {dispatch}
+    >
         <Image source = { image } style = { EachFarmerStyles.image } accessiblityLabel = { name } />
         <View style = { EachFarmerStyles.info }>
             <View>
--- src/screens/Home/components/FarmersComponent/Farmers/Farmers.js
+++ src/screens/Home/components/FarmersComponent/Farmers/Farmers.js
@@ -6,22 +6,21 @@ import useFarmers from '../../../../../hooks/useFarmers';
 import EachFarmer from '../EachFarmer/EachFarmer';
 import { FarmersStyles } from './styles';
 
-const HeaderList = ( Header, title ) => {
-    return <>
-        <Header />
-        <Text style = { FarmersStyles.title }>{ title }</Text>
-    </>;
-};
-
 export default function Farmers({ header: Header }) {
     const [title, list ] = useFarmers();
 
-    return (
-        <FlatList
+    // eslint-disable-next-line react/no-unstable-nested-components
+    const HeaderList = () => {
+        return <>
+            <Header />
+            <Text style = { FarmersStyles.title }>{ title }</Text>
+        </>;
+    };
+
+    return <FlatList
         data = {list}
         renderItem = {({item}) => <EachFarmer {...item} />}
         keyExtractor= {(name) => name}
-        ListHeaderComponent = {() => <HeaderList Header = { Header } title = { title } />}
-        />
-    );
+        ListHeaderComponent = {HeaderList}
+    />;
 }
